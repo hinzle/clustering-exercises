bb231337faf345a30fe4ce12eae0556a27f6684d
hinzle <bosque4d@hotmail.com>
2022-04-05 15:39:23 -0400

    ✅ gitignore

--- utils/prepare.py
+++ utils/prepare.py
@@ -3,9 +3,9 @@
 from utils.imports import *
 
 
-def handle_missing_values(df, prop_required_column, prop_required_row):
-    n_required_column = round(df.shape[0] * prop_required_column)
-    n_required_row = round(df.shape[1] * prop_required_row)
+def handle_missing_values(df, percent_missing_col, percent_missing_row):
+    n_required_column = round(df.shape[0] * percent_missing_col)
+    n_required_row = round(df.shape[1] * percent_missing_row)
     df = df.dropna(axis=0, thresh=n_required_row)
     df = df.dropna(axis=1, thresh=n_required_column)
     return df
